67957b12da3da0c411a924901f5f690343c00b63
programas2024 <joacoxx2340@gmail.com>
2025-08-22 15:24:36 -0500

    aver

--- leaderboard.js
+++ leaderboard.js
@@ -315,8 +315,8 @@ async function showPlayerDetails(supabase, targetUserId, currentUserId, playerRa
 let socialMediaButtonHtml = '';
 if (hasSocialMedia) {
     socialMediaButtonHtml = `
-        <button class="fas fa-icons" style="position: absolute; top: 5px; right: 15px; background: linear-gradient(to right, #8a2387, #e94057, #f27121); color: white; border: none; border-radius: 50%; width: 40px; height: 40px; cursor: pointer; display: flex; align-items: center; justify-content: center; font-size: 18px; z-index: 1000;">
-            
+        <button class="social-media-btn" style="position: absolute; top: 5px; right: 15px; background: linear-gradient(to right, #376de2ff, #e94057, #ff9351ff); color: white; border: none; border-radius: 50%; width: 40px; height: 40px; cursor: pointer; display: flex; align-items: center; justify-content: center; font-size: 18px; z-index: 1000;">
+            <i class="fas fa-icons"></i>
         </button>
     `;
 }
